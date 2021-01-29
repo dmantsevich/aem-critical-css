c7d51ffc40187d29b14915deca854e5ae1816f5a
dmantsevich <dmantsevich@exadel.com>
2021-01-30 02:11:11 +0300

    1. Add initial functionality 2. Add analyzing templates functions 3. Add simple css compilation

---
+++ index.js
@@ -0,0 +1,6 @@
+const merge = require('lodash.merge');
+
+module.exports.process = async (customConfig = {}) => {
+  const config = merge({}, require('./lib/config')(), customConfig);
+  await require('./lib/process')(config);
+}
