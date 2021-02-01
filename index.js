b69913d779d2b9cc405299e4086ec2f9f047cbff
dmantsevich <dmantsevich@exadel.com>
2021-02-02 01:59:02 +0300

    13. Bugfixing 14. Add minification

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const merge = require('lodash.merge');
 module.exports.process = async (customConfig = {}) => {
   const config = merge({}, require('./lib/config')(), customConfig);
   await require('./lib/process')(config);
-}
+};
