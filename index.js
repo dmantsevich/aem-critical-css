d8a0165cf727519e2a32ba71419c42a17495a7bb
dmantsevich <dmantsevich@exadel.com>
2021-02-04 00:34:53 +0300

    19. Bugfixes 20. Add compile option 21. Update public exports

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const merge = require('lodash.merge');
 
+Object.assign(module.exports, require('./lib/const'));
+
 module.exports.process = async (customConfig = {}) => {
   const config = merge({}, require('./lib/config')(), customConfig);
   await require('./lib/process')(config);
